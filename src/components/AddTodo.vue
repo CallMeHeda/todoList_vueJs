d62c7caa85157d59af6346090fe16ddec6b15953
FatimaElamri <fatimaelamri789@gmail.com>
2022-11-21 00:23:38 +0100

    todo list sans css

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="todoToAdd">
+    <a href="#" v-if="todoToAdd != ''" @click="handleClick">&nbsp; Ajouter</a>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const todoToAdd = ref("");
+const emit = defineEmits(['newTodo']);
+
+function handleClick() {
+  emit('newTodo', todoToAdd.value);
+  todoToAdd.value = "";
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,42 @@
+<template>
+  <AddTodo @newTodo="addTodo" />
+  <p v-for="todo in todos" @click="checkTodo"><span v-if="todo.id != 0">({{ todo.id }}) - {{ todo.tache }}</span></p>
+</template>
+
+<script lang="ts">
+
+import AddTodo from './AddTodo.vue';
+
+const todos: [{ id: Number, tache: String }] = [{ id: 0, tache: "" }]
+
+export default ({
+  data() {
+    return {
+      todos
+    }
+  },
+  components: {
+    AddTodo
+  },
+  methods: {
+    checkTodo(e: any) {
+      if (!e.target.firstChild.parentElement.classList.contains("line-through")) {
+        e.target.firstChild.parentElement.classList.add("line-through");
+      } else {
+        e.target.firstChild.parentElement.classList.remove("line-through");
+      }
+    },
+    addTodo(message: string) {
+      let id = this.todos.length;
+      this.todos.push({ id: id, tache: message });
+    }
+  },
+});
+
+</script>
+
+<style scoped>
+.line-through {
+  text-decoration: line-through;
+}
+</style>
